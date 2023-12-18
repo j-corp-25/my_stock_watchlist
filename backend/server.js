11158c835c375933c325a57d9f5c658dcd295e40
Jordy <jordycorporan95@gmail.com>
2023-12-18 10:15:47 -0500

    add custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 4000
 const app = express()
 
@@ -7,5 +8,5 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/watchlist', require('./routes/watchlistRoutes'))
-
+app.use(errorHandler)
 app.listen(port,() => console.log(`Server started on port ${port}`))
