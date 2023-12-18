5945d85ee489c72cdf505f07655ee8f27f367b80
Jordy <jordycorporan95@gmail.com>
2023-12-18 11:27:33 -0500

    tested create and get watchlists: working

--- backend/controllers/watchlistController.js
+++ backend/controllers/watchlistController.js
@@ -1,28 +1,35 @@
-const asyncHandler = require('express-async-handler')
-const Watchlist = require('../models/watchlistModel')
+const asyncHandler = require("express-async-handler");
+const Watchlist = require("../models/watchlistModel");
 
 // @desc GET watchlists
 // @route GET /api/watchlists
 // @access Private
 const getWatchlist = asyncHandler(async (req, res) => {
-  
+  const watchlists = await Watchlist.find();
+  res.status(200).json(watchlists);
 });
 
 // @desc Create watchlist
 // @route POST /api/watchlist
 // @access Private
 const createWatchlist = asyncHandler(async (req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error("Please add a Ticker symbol")
-    }
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("Watchlist name cannot be empty");
+  }
+  const watchlist = await Watchlist.create({
+    name: req.body.name,
+    description: req.body.description,
+    tickers: req.body.tickers,
+  });
+
+  res.status(200).json(watchlist);
 });
 
 // @desc Update watchlists
 // @route PATCH /api/watchlists/:id
 // @access Private
 const updateWatchlist = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "im from the controller update" });
 });
 
 // @desc Delete watchlists
