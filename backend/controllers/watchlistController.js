5c1e1de9c2c2a4b1319f63991ff97fe24f6c2833
Jordy <jordycorporan95@gmail.com>
2023-12-18 10:26:36 -0500

    watchlist routes and controller

--- backend/controllers/watchlistController.js
+++ backend/controllers/watchlistController.js
@@ -3,33 +3,33 @@ const asyncHandler = require('express-async-handler')
 // @desc GET watchlists
 // @route GET /api/watchlists
 // @access Private
-const getWatchlist = async (req, res) => {
+const getWatchlist = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "im from the get all controller" });
-};
+});
 
 // @desc Create watchlists
 // @route POST /api/watchlists
 // @access Private
-const createWatchlist = async (req, res) => {
+const createWatchlist = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error("Please add a Ticker symbol")
     }
-};
+});
 
 // @desc Update watchlists
 // @route PATCH /api/watchlists/:id
 // @access Private
-const updateWatchlist = async (req, res) => {
+const updateWatchlist = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "im from the controller update" });
-};
+});
 
 // @desc Delete watchlists
 // @route DELETE /api/watchlists/:id
 // @access Private
-const deleteWatchlist = async (req, res) => {
+const deleteWatchlist = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "im from the controller delete" });
-};
+});
 
 module.exports = {
   getWatchlist,
