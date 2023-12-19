aab83273e1cfcbb3bf34c05f1dafb2127da90e60
Jordy <jordycorporan95@gmail.com>
2023-12-18 19:36:25 -0500

    add helmet middleware for security

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const helmet = require("helmet")
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 4000
-connectDB()
 const app = express()
 
 
+
+app.use(helmet)
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/watchlist', require('./routes/watchlistRoutes'))
 app.use(errorHandler)
 app.listen(port,() => console.log(`Server started on port ${port}`))
+
+connectDB()
