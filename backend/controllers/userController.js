92a127fa5d8c13204aa3ae1c50778f4abe109998
Jordy <jordycorporan95@gmail.com>
2023-12-22 13:40:39 -0500

    working on login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,32 +21,44 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password,salt)
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const user = await User.create({
     name,
     email,
-    password: hashedPassword
-  })
+    password: hashedPassword,
+  });
 
-  if(user){
+  if (user) {
     res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+// @desc Authenticate a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if(user && (await bcrypt.compare(password, user.password))){
+    res.json({
         _id: user.id,
         name: user.name,
         email: user.email
     })
   }else {
     res.status(400)
-    throw new Error('Invalid user data')
+    throw new Error('Invalid credentials')
   }
-
-});
-// @desc Authenticate a user
-// @route POST /api/users/login
-// @access Public
-const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
 });
 // @desc GET user data
 // @route GET /api/users/me
