a7934dd245511a1ec10a98c52189a65188477e66
Jordy <jordycorporan95@gmail.com>
2023-12-22 16:39:52 -0500

    testing webtokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateJWT(user._id),
     });
   } else {
     res.status(400);
@@ -49,23 +50,46 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if(user && (await bcrypt.compare(password, user.password))){
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
-        _id: user.id,
-        name: user.name,
-        email: user.email
-    })
-  }else {
-    res.status(400)
-    throw new Error('Invalid credentials')
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateJWT(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
   }
 });
 // @desc GET user data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data display" });
+  //? deconstruction approach to getting user document
+  //   const {_id, name, email} = await User.findById(req.user.id)
+
+  //   res.status(200).json({
+  //     id: _id,
+  //     name,
+  //     email
+  //   })
+
+  // storing whole document in a variable pattern
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  });
 });
+
+// Generate JWT token for current user
+
+const generateJWT = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 module.exports = {
   registerUser,
