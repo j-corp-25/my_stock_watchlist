98043540f2be34b773148ce0d3ff1223f16fe6c5
Jordy <jordycorporan95@gmail.com>
2023-12-23 02:25:49 -0500

    work on watchlist component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="logo">
+        <Link to="/">WatchlistSetter</Link>
+      </div>
+      <ul>
+        
+      </ul>
+    </header>
+  );
 }
 
-export default Header
-
+export default Header;
