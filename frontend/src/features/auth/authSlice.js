756f26d731ce07b9394826d42bebcf3bf1487f00
Jordy <jordycorporan95@gmail.com>
2023-12-25 23:58:15 -0500

    connect proxy

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
+import authService from "./authService";
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
 const initialState = {
@@ -10,6 +10,24 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+// register user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+        return thunkAPI.rejectWithValue(message)
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -26,5 +44,5 @@ export const authSlice = createSlice({
   extraReducers: () => {},
 });
 
-export const { reset } = authSlice.actions
-export default authSlice.reducer
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
